c1dc1885b03c267ed432b7f9ae99d5f99e58229d
takeinpl <155150555+takeinpl@users.noreply.github.com>
2024-04-13 01:01:54 +0900

    Update UmaDataHelper.cs

--- UmaMusumeData/UmaDataHelper.cs
+++ UmaMusumeData/UmaDataHelper.cs
@@ -8,7 +8,7 @@ namespace UmaMusumeData
 {
     public static class UmaDataHelper
     {
-        private static readonly string localLow = Path.Combine(Environment.GetPathRoot(Environment.SystemDirectory) + "kakaogames");
+        private static readonly string localLow = @"C:\kakaogames";
         private static readonly string umaMusumeDirectory = Path.Combine(localLow, "umamusume", "client", "Assets");
         private static readonly string dataDirectory = Path.Combine(umaMusumeDirectory, "dat");
         private static readonly string metaFile = Path.Combine(umaMusumeDirectory, "meta");
