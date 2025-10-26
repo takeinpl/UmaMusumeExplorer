f4f667f14ca0968f8d3736463540aedf6a89efbf
Jacob Tarun <jacobtarun782@outlook.com>
2025-10-26 20:27:41 +0800

    Fix crash with no warning (#27)

--- UmamusumeData/DataDirectories/DmmDataDirectory.cs
+++ UmamusumeData/DataDirectories/DmmDataDirectory.cs
@@ -19,6 +19,8 @@ namespace UmamusumeData.DataDirectories
 
         public override bool Exists()
         {
+            if (!File.Exists(dmmConfigPath)) return false;
+
             string configJson = File.ReadAllText(dmmConfigPath);
 
             JsonNode? json = JsonNode.Parse(configJson);
