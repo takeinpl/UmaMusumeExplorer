bf7ed71912bf2a870342eb4abd084ac7c1224184
takeinpl <155150555+takeinpl@users.noreply.github.com>
2025-11-10 01:47:28 +0900

    Add Korean Client Directory

---
+++ UmamusumeData/DataDirectories/KakaoDataDirectory.cs
@@ -0,0 +1,20 @@
+﻿namespace UmamusumeData.DataDirectories
+{
+    internal class KakaoDataDirectory : DataDirectory
+    {
+        private static readonly string kakaogamesDirectory = "C:\kakaogames";
+        private static readonly string umamusumeDirectory = Path.Combine(kakaogamesDirectory, "umamusume", "client", "Assets");
+
+        public override string Name => "Default (AppData)";
+
+        public override bool Exists()
+        {
+           if (CheckDirectory(dataDirectory))
+            {
+                DataDirectoryPath = dataDirectory;
+                return true;
+            }
+            return false;
+        }
+    }
+}
