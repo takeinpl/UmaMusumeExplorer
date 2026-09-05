2a8cbc36ab24a0fd734315c3e86768f20d91c6e1
Jacob Tarun <jacobtarun782@outlook.com>
2026-09-05 16:34:16 +0800

    Cleanup

--- UmamusumeData/UmaDataHelper.cs
+++ UmamusumeData/UmaDataHelper.cs
@@ -7,10 +7,10 @@ namespace UmamusumeData
     public static class UmaDataHelper
     {
         private static readonly string[] keys =
-        {
+        [
             "9c2bab97bcf8c0c4f1a9ea7881a213f6c9ebf9d8d4c6a8e43ce5a259bde7e9fd",
             "c753a5e8f5f78294f7fef57df4a14ffbf9a896cea1d4e09947e0d904e7fde8eaf0"
-        };
+        ];
 
         private static string key = "";
 
